f4dd195fa47ab03a21845625d4cb40e1d503b70f
Ken Scanlon <40871029+scanlon771@users.noreply.github.com>
2019-08-07 16:45:41 -0700

    8/7/19 new commit

---
+++ script3.js
@@ -0,0 +1,209 @@
+// Function constructor
+/*
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function () {
+    console.log(2019 - this.yearOfBirth);
+}
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
+*/
+/*
+var Cat = function (name, yearOfBirth, color, sex) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.color = color;
+    this.sex = sex;
+}
+
+var crumpet = new Cat('Crumpet', 2017, 'tuxedo', 'male');
+var biscuit = new Cat('Biscuit', 2019, 'black', 'female');
+var yubi = new Cat('Yubi', 2008, 'calico', 'female');
+var kyomi = new Cat('Kyomi', 2012, 'white', 'female');
+
+Cat.prototype.calculateAge = function () {
+    console.log(2019 - this.yearOfBirth);
+}
+
+Cat.prototype.whichSex = function () {
+    console.log(this.sex);
+}
+
+crumpet.calculateAge();
+crumpet.whichSex();
+biscuit.calculateAge();
+biscuit.whichSex();
+*/
+
+// Object.create
+/*
+var personProto = {
+    calculateAge: function() {
+        console.log(2019 - this.yearOfBirth);
+    }
+}
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+    name: { value: 'Jane' },
+    yearOfBirth: { value: 1969},
+    job: { value: 'designer'}
+});
+*/
+
+// Primitives vs Objects
+
+/*
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+// Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1);
+console.log(obj2);
+
+// Function 
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change (a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+*/
+/*
+///////////////////////////////////////////////////////////////////
+// Lecture: Passing functions as arguments
+
+var year = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrResult = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrResult.push(fn(arr[i]));
+    }
+    return arrResult;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+}
+
+var ages = arrayCalc(year, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
+*/
+
+////////////////////////////////////////////////////////////
+// Lecture: Functions returning functions
+
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is>');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+                console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+                console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('John');
+designerQuestion('Jane');
+designerQuestion('Mark');
+designerQuestion('Mike');
+
+interviewQuestion('teacher')('Mark');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
