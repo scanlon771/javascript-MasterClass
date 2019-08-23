9969d8f5be35d04a4c70e2bb381ff92c2c2c71c8
Ken Scanlon <40871029+scanlon771@users.noreply.github.com>
2019-08-23 16:32:01 -0700

    8/23/19 new commit

--- script3.js
+++ script3.js
@@ -155,11 +155,11 @@ console.log(rates);
 
 ////////////////////////////////////////////////////////////
 // Lecture: Functions returning functions
-
+/*
 function interviewQuestion(job) {
     if (job === 'designer') {
         return function(name) {
-            console.log(name + ', can you please explain what UX design is>');
+            console.log(name + ', can you please explain what UX design is?');
         }
     } else if (job === 'teacher') {
         return function(name) {
@@ -182,18 +182,128 @@ designerQuestion('Mark');
 designerQuestion('Mike');
 
 interviewQuestion('teacher')('Mark');
+*/
+////////////////////////////////////////////////////////////
+// Lecture: IIFE
 
+/*
+function game() {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+}
+game();
+*/
+/*
+(function () {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+})();
 
+//console.log(score);
 
+(function (goodLuck) {
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodLuck);
+})(5);
+*/
 
+//////////////////////////////////////////////
+// Lecture: Closures
+/*
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a)
+    }
+}
 
+var retirementUS = retirement(66);
+retirementUS(1990);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
 
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
 
+//retirement(66)(1990);
 
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
 
+interviewQuestion('teacher')('John');
+*/
 
+///////////////////////////////////////////////////////
+// Lecture: Bind, call and apply
 
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m '
+            + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+        } else if (style === 'friendly') {
+            console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m a ' + this.job 
+            + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.' );
+        }
+    }
+}
 
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+}
+
+john.presentation('formal', 'morning');
+
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+//john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrResult = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrResult.push(fn(arr[i]));
+    }
+    return arrResult;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(limit, el) {
+    return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(fullJapan);
+console.log(ages);
 
 
 
